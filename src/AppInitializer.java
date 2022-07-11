b1303729f6b91b48b0753ba6542b71a66f989382
Rajith Sanjaya <sanjayasenanayaka11@gmail.com>
2022-07-11 23:26:26 +0530

    add files

---
+++ src/AppInitializer.java
@@ -0,0 +1,19 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * @PROJECT SuperMarketMVC
+ * @Author Rajith Sanjaya
+ * @Date 2022 Jul 11
+ **/
+public class AppInitializer extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
---
+++ src/controller/CustomerController.java
@@ -0,0 +1,9 @@
+package controller;
+
+/**
+ * @PROJECT SuperMarketMVC
+ * @Author Rajith Sanjaya
+ * @Date 2022 Jul 11
+ **/
+public class CustomerController {
+}
